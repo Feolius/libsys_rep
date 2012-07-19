e7c561f7115c2b5e4d68518bfce49ae192f5e8f0
Alexander <alexander.zyryanov44@gmail.com>
2012-07-19 21:37:58 +0700

    node page

---
+++ sites/all/modules/collection_layout/timeline/timeline_settings.js
@@ -0,0 +1,7 @@
+var timeline_config = {
+    width: 	"100%",
+    height: "500px",
+    source: 'timeline-json',
+    css: 	'timeline/compiled/css/timeline.css',
+    js: 	'timline/compiled/js/timeline-min.js'
+}
