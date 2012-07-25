e2b1640f318d69cbac5c6c27ce5603e0007486a4
feolius <feolius@gmail.com>
2012-07-25 23:42:48 +0700

    Fixed bug with teasers and multiple views of book_field

--- sites/all/modules/book_field/book_field.js
+++ sites/all/modules/book_field/book_field.js
@@ -1,36 +1,51 @@
 (function ($) {   
     $(function(){
-        var path = Drupal.settings.book_field.path;    
-        var fp = new FlexPaperViewer(	
-            '/sites/all/modules/book_field/FlexPaperViewer',
-            'viewerPlaceHolder', {
-                config : {
-                    SwfFile : path,
-                    Scale : 0.6, 
-                    ZoomTransition : 'easeOut',
-                    ZoomTime : 0.5,
-                    ZoomInterval : 0.2,
-                    FitPageOnLoad : true,
-                    FitWidthOnLoad : false,
-                    FullScreenAsMaxWindow : false,
-                    ProgressiveLoading : false,
-                    MinZoomSize : 0.2,
-                    MaxZoomSize : 5,
-                    SearchMatchAll : false,
-                    InitViewMode : 'Portrait',
+        var paths = Drupal.settings.book_field.paths;    
+        var length = paths.length;
+        var i = 0;
+        var placeHolders = $('p.viewerPlaceHolder');
+        placeHolders.each(function(index){
+            $(this).attr('id', 'viewerPlaceHolder_' + index); 
+        });
+        for (var i = 0; i < length; i++){
+            var id = 'viewerPlaceHolder_' + i.toString();
+            var res = $.find('#' + id);
+            if(res.length != 0){
+                var path = paths[i];
+                var fp = new FlexPaperViewer(	
+                    '/sites/all/modules/book_field/FlexPaperViewer',
+                    id, {
+                        config : {
+                            SwfFile : path,
+                            Scale : 0.6, 
+                            ZoomTransition : 'easeOut',
+                            ZoomTime : 0.5,
+                            ZoomInterval : 0.2,
+                            FitPageOnLoad : true,
+                            FitWidthOnLoad : false,
+                            FullScreenAsMaxWindow : false,
+                            ProgressiveLoading : false,
+                            MinZoomSize : 0.2,
+                            MaxZoomSize : 5,
+                            SearchMatchAll : false,
+                            InitViewMode : 'Portrait',
 						 
-                    ViewModeToolsVisible : true,
-                    ZoomToolsVisible : true,
-                    NavToolsVisible : true,
-                    CursorToolsVisible : true,
-                    SearchToolsVisible : true,
+                            ViewModeToolsVisible : true,
+                            ZoomToolsVisible : true,
+                            NavToolsVisible : true,
+                            CursorToolsVisible : true,
+                            SearchToolsVisible : true,
   						
-                    localeChain: 'en_US'
-                }
-            });
+                            localeChain: 'en_US'
+                        }
+                    });
 						 
-    function onDocumentLoadedError(errMessage){
-        $('#viewerPlaceHolder').html("Error displaying document. Make sure the conversion tool is installed and that correct user permissions are applied to the SWF Path directory");
-    }
+                function onDocumentLoadedError(errMessage){
+                    $('#viewerPlaceHolder').html("Error displaying document. Make sure the conversion tool is installed and that correct user permissions are applied to the SWF Path directory");
+                }
+            
+            }
+        }
+        
     });
 })(jQuery);
