e30449a8ae6f54b868ef290df72cd8482a79f545
alexanderzyryanov <alexander.zyryanov44@gmail.com>
2012-08-11 07:49:15 +0000

    delete custom module from core of drupal

--- modules/collection_layout/js/m-radio.js
+++
@@ -1,8 +0,0 @@
-(function ($) {
-    $(function() {
-        $(".toggle-buttons > .m-btn").click(function() {
-            $(this).siblings(".m-btn").removeClass("active");
-            $(this).addClass("active");
-        });
-    })
-})(jQuery)
