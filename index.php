4da7c7684666ab2bd358d0de8499e140ed01b423
root <root@ip-10-251-93-175.eu-west-1.compute.internal>
2012-08-28 08:16:10 +0000

    enable php errors in index.php

--- index.php
+++ index.php
@@ -1,5 +1,9 @@
 <?php
+error_reporting(E_ALL);
 
+ini_set('display_errors', true);
+
+ini_set('error_reporting',  E_ALL);
 /**
  * @file
  * The PHP page that serves all page requests on a Drupal installation.
