a3823c5552e9629b776bbf809c61545036e581dd
Bazhen Rzheutskiy <bazhen.rzheutsky@gmail.com>
2012-11-07 20:07:17 +0700

    collection arrows module

---
+++ sites/all/modules/custom/collection_arrows/js/node_slider_buttons.js
@@ -0,0 +1,48 @@
+(function ($) {
+    $(document).ready(function () {
+        var arrow_left = $('<div class="collection_layout_slider_button_left">' +
+                '<a class="m-icon-big-swapleft active" ' +
+                'href="/node/' + Drupal.settings.collection_arrows.previous_node + '"/>' +
+                '</div>'),
+            arrow_right = $('<div class="collection_layout_slider_button_right">' +
+                '<a class="m-icon-big-swapright active" ' +
+                'href="/node/' + Drupal.settings.collection_arrows.next_node + '"/>' +
+                '</div>');
+        $('body').append(arrow_left);
+        $('body').append(arrow_right);
+    });
+    collectionLayout = {
+        reposition:function (eventObject) {
+            $(".collection_layout_slider_button_right").offset({
+                top:eventObject.newtop == undefined ? 500 : eventObject.newtop,
+                left:$("#content-inside").offset().left + $("#content-inside").width()
+            })
+            $(".collection_layout_slider_button_left").offset({
+                top:eventObject.newtop == undefined ? 500 : eventObject.newtop,
+                left:$("#content-inside").offset().left - $(".collection_layout_slider_button_left").width()
+            })
+        },
+        scrollReposition:function (newtop) {
+            $(".collection_layout_slider_button_left").offset({
+                top:newtop == undefined ? 500 : newtop
+            })
+            $(".collection_layout_slider_button_right").offset({
+                top:newtop == undefined ? 500 : newtop
+            })
+        }
+    };
+    $(".collection_layout_slider_button_left a").ready(function () {
+        $(".collection_layout_slider_button_left a").text('');
+    });
+    $(".collection_layout_slider_button_right a").ready(function () {
+        $(".collection_layout_slider_button_right a").text('');
+    });
+    $(collectionLayout.reposition);
+    $(window).resize(collectionLayout.reposition);
+    $(document).scroll(function (eventObject) {
+        if ($(this).scrollTop() + 500 < $("#content-inside").offset().top + $("#content-inside").height())
+            collectionLayout.scrollReposition($(this).scrollTop() + 500);
+    });
+
+})(jQuery);
+
