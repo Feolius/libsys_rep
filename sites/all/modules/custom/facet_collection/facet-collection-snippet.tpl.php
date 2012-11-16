59b4acb655ccb3fc046f30e0c54ff951629ef09a
feolius <feolius@gmail.com>
2012-11-16 14:35:13 +0700

    Add template for search snippets

--- sites/all/modules/custom/facet_collection/facet-collection-snippet.tpl.php
+++ sites/all/modules/custom/facet_collection/facet-collection-snippet.tpl.php
@@ -1,3 +1,17 @@
 <?php
 
-print $text;
+/**
+ * Template file for search result snippets un facet collection page
+ */
+?>
+
+<?php if (isset($image_path)): ?>
+<div class="facet-collection-snippet-thumb">
+    <img src= <?php print $image_path ?>>
+</div>
+<?php endif; ?>
+<?php if (isset($text)): ?>
+<div class="facet-collection-snippet-text">
+     <?php print t($text) ?>
+</div>
+<?php endif; ?>
