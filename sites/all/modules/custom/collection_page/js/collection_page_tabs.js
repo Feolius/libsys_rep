8b6662c94a41787afe24ff84c9c5d5e2d3a07918
Bazhen Rzheutskiy <bazhen.rzheutsky@gmail.com>
2012-11-20 12:59:56 +0700

    add collection_page module

--- sites/all/modules/custom/collection_page/js/collection_page_tabs.js
+++ sites/all/modules/custom/collection_page/js/collection_page_tabs.js
@@ -2,7 +2,6 @@
     Drupal.behaviors.collection_page_tabs = {
         attach:function () {
             $("#collection_page_tabs").tabs();
-           // $("#collection_page_tabs").tabs("select", 1);
         }
     }
 
--- sites/all/modules/custom/collection_page/theme/collection-page.tpl.php
+++ sites/all/modules/custom/collection_page/theme/collection-page.tpl.php
@@ -1,12 +1,20 @@
 <div id="collection_page_tabs">
     <ul>
-        <li><a href="#tabs-1"><?php print $elements['#timeline']['#title']?></a></li>
-        <li><a href="#tabs-2"><?php print $elements['#location']['#title']?></a></li>
-        <li><a href="#tabs-3"><?php print $elements['#standart']['#title']?></a></li>
+        <li><a href="#tabs-1"><?php print $elements['#standart']['#title']?></a></li>
+        <li><a href="#tabs-2"><?php print $elements['#timeline']['#title']?></a></li>
+        <li><a href="#tabs-3"><?php print $elements['#location']['#title']?></a></li>
 
     </ul>
-    <div id="tabs-1"><?php print $elements['#timeline']['#content']['#timeline_iframe']?></div>
-    <div id="tabs-2"><?php print $elements['#location']['#content']['#location_view']?></div>
-    <div id="tabs-3"><?php print $elements['#standart']['#content']['#standart_view']?></div>
+    <div id="tabs-1"><?php print $elements['#standart']['#content']['#standart_view']?></div>
+    <div id="tabs-2">
+        <?php print $elements['#timeline']['#content']['#timeline_iframe']?>
+        <?php print $elements['#pager']; ?>
+    </div>
+    <div id="tabs-3">
+        <?php print $elements['#location']['#content']['#location_view']?>
+        <?php print $elements['#pager']; ?>
+    </div>
+
+
 </div>
 
