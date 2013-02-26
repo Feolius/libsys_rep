33bdfe355ad0e2903126f84b36c757d57506d2f7
Bazhen Rzheutskiy <bazhen.rzheutsky@gmail.com>
2013-02-26 13:16:22 +0700

    change the version of openlayers module to dev version, minor fixes in modules album_audio and id3_widget module

--- sites/all/modules/contrib/openlayers/plugins/layer_types/openlayers_layer_type_tms.js
+++ sites/all/modules/contrib/openlayers/plugins/layer_types/openlayers_layer_type_tms.js
@@ -9,11 +9,10 @@
  */
 Drupal.openlayers.layer.tms = function(title, map, options) {
   var styleMap = Drupal.openlayers.getStyleMap(map, options.drupalID);
-  options.isBaseLayer = options.baselayer;
     if (options.maxExtent !== undefined) {
       options.maxExtent = new OpenLayers.Bounds.fromArray(options.maxExtent);
     }
-    options.projection = 'EPSG:' + options.projection;
+    options.projection = new OpenLayers.Projection(options.projection);
     var layer = new OpenLayers.Layer.TMS(title, options.base_url, options);
     layer.styleMap = styleMap;
     return layer;
