bc9f7e432b1537e48147e187067c57a8d171bd2a
feolius <feolius@gmail.com>
2013-02-28 16:46:50 +0700

    Fixed bug with closed accordion when timeline run first

--- sites/all/modules/custom/collection_page/js/collection_page_accordion.js
+++ sites/all/modules/custom/collection_page/js/collection_page_accordion.js
@@ -1,7 +1,7 @@
 (function ($) {
   Drupal.behaviors.collectionPageAccordion = {
     attach: function (context, settings) {
-       var active = Drupal.settings.collection_book.active;
+       var active = Drupal.settings.collection_page.active;
       $( '#colection-search-results-accordion' ).accordion({
         collapsible: true,
         active: active
