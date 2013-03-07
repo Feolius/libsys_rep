cc960a9eb408417e88d18fb6b12fc087bf8994fa
feolius <feolius@gmail.com>
2013-03-07 16:08:35 +0700

    Migration module first production version

--- sites/all/modules/custom/collection_page/js/collection_page_thumbnails.js
+++ sites/all/modules/custom/collection_page/js/collection_page_thumbnails.js
@@ -1,15 +1,21 @@
 (function($) {
   Drupal.behaviors.collectionPageThumbnails = {
     attach: function(context, settings) {
-      //Coefficient of scaling
-      var coefficient = Drupal.settings.collection_page.coefficient;      
+      var height = Drupal.settings.collection_page.height;
+      var lowHeight = height * 2 / 3;
      
-      $(".collection-thumbnail").once('collectionPageThumbnails').each(function() {        
-        $(this).height($(this).height() / coefficient);
-        $(this).width($(this).width()/ coefficient);        
+      $(".collection-thumbnail").once('collectionPageThumbnails').each(function() {
+        var height = $(this).height();
+        var width = $(this).width();
+        $(this).height(height * 2 / 3);
+        $(this).width(width * 2 / 3);
+        var parent = $(this).parent();
+        parent.width(width * 2 / 3);
+        parent.height(height * 2 / 3);
       });
 
-      $("#collection-upper-thumbnail", context).hover(function() {
+
+      $("#collection-upper-thumbnail", context).once('collectionPageThumbnails').hover(function() {
       }, function() {
         $("#collection-thumbnail-upper-container").css("display", "none");
         
@@ -24,27 +30,35 @@
         var initialLeft = $(this).position().left - parentPaddingLeft;
         var initialWidth = $(this).width();
         var initialHeight = $(this).height();
-        var width = initialWidth * coefficient;       
+        var width = initialWidth * 1.5;
+        height = initialHeight * 1.5;
+        var padding = parseInt($("#collection-thumbnail-upper-container").css("padding-left"));
         $("#collection-thumbnail-upper-container").css("width", initialWidth);
         $("#collection-thumbnail-upper-container").css("top", $(this).position().top - parentPaddingTop);
-        $("#collection-thumbnail-upper-container").css("left", initialLeft);       
-        $("#collection-upper-thumbnail").attr("src", $(this).attr("src"));        
+        $("#collection-thumbnail-upper-container").css("left", initialLeft);
+        //$('#collection-upper-thumbnail').html("<div style='background-color:black; width: 100%; height: 100%;'></div>");
+        $("#collection-upper-thumbnail").attr("src", $(this).attr("src"));
+        //$("#collection-upper-thumbnail").width(initialWidth);
+       // $("#collection-upper-thumbnail").height(initialHeight);
         $("#collection-upper-thumbnail-link").attr("href", nodeLink);
-        $("#collection-thumbnail-upper-container").css("display", "none");        
+        $("#collection-thumbnail-upper-container").css("display", "none");
+        var n = 0;
         if ($("#collection-thumbnail-upper-container").css("display") === "none") {
           $("#collection-thumbnail-upper-container").css("display", "block");
           $("#collection-thumbnail-upper-container").animate({
             width: width
           }, {
             duration: 300,
-            queue: false,            
+            queue: false,
+            
             complete: function() {
               $(this).css('height', '');
               $("#collection-thumbnail-metainfo").html(metainfo);
               $("#collection-thumbnail-metainfo").width(1.5 * initialWidth);
             },
             step: function(now, fx) {             
-              var left = initialLeft - (now - initialWidth) * 0.5;
+              var cur = $("#collection-thumbnail-upper-container").width() + 10;            
+              var left = initialLeft - (cur - initialWidth) * 0.5;
               var height = initialHeight*now/initialWidth;
               $(this).height(height);            
               if (left < 0) {
@@ -61,8 +75,11 @@
         } else {
           $("#collection-upper-thumbnail").attr("src", $(this).attr("src"));
         }
-      }, function() {});
+
+      }, function() {
+
+      });
     }
-  };
+  }
 })(jQuery);
 
