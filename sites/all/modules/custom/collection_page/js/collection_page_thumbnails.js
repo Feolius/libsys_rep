5bee75cfa5ad88bfa597fdbd56ea510ee12c877a
feolius <feolius@gmail.com>
2013-03-07 11:59:05 +0700

    Rewrited collection thumbnail view

--- sites/all/modules/custom/collection_page/js/collection_page_thumbnails.js
+++ sites/all/modules/custom/collection_page/js/collection_page_thumbnails.js
@@ -1,59 +1,68 @@
-(function ($) {
-  Drupal.behaviors.collectionThumbnailsView = {
-    attach: function (context, settings) {
-      var height = Drupal.settings.collection_page.height;
-      //Height for visually compressed images
-      var lowHeight = height*2/3;
-       $(".collection-thumbnail").css("height", lowHeight);
-       $(".collection-upper-thumbnail").css("height", lowHeight);
-      $("#collection-upper-thumbnail").hover(function(){
+(function($) {
+  Drupal.behaviors.collectionPageThumbnails = {
+    attach: function(context, settings) {
+      //Coefficient of scaling
+      var coefficient = Drupal.settings.collection_page.coefficient;      
+     
+      $(".collection-thumbnail").once('collectionPageThumbnails').each(function() {        
+        $(this).height($(this).height() / coefficient);
+        $(this).width($(this).width()/ coefficient);        
+      });
 
-        },function(){
-          $("#collection-thumbnail-upper-container").css("display", "none");
-          $("#collection-upper-thumbnail").css("height", lowHeight);
-          $("#collection-thumbnail-metainfo").html("");
-        });
-      $(".collection-thumbnail").hover(function(){
+      $("#collection-upper-thumbnail", context).hover(function() {
+      }, function() {
+        $("#collection-thumbnail-upper-container").css("display", "none");
+        
+        $("#collection-thumbnail-metainfo").html("");
+      });
+      $(".collection-thumbnail", context).hover(function() {
+        $("#collection-thumbnail-upper-container").css("display", "block");
         var metainfo = $(this).parent().find(".collection-thumbnail-metainfo").html();
         var nodeLink = $(this).parent().find(".collection-thumbnail-node-link").val();
-        var initialLeft = $(this).position().left;
+        var parentPaddingLeft = parseInt($(this).parent().css("padding-left"));
+        var parentPaddingTop = parseInt($(this).parent().css("padding-top"));
+        var initialLeft = $(this).position().left - parentPaddingLeft;
         var initialWidth = $(this).width();
-        var padding = parseInt($("#collection-thumbnail-upper-container").css("padding-left"));
-        $("#collection-thumbnail-upper-container").css("top", $(this).position().top - padding);
-        $("#collection-upper-thumbnail").attr("src", $(this).attr("src"));
+        var initialHeight = $(this).height();
+        var width = initialWidth * coefficient;       
+        $("#collection-thumbnail-upper-container").css("width", initialWidth);
+        $("#collection-thumbnail-upper-container").css("top", $(this).position().top - parentPaddingTop);
+        $("#collection-thumbnail-upper-container").css("left", initialLeft);       
+        $("#collection-upper-thumbnail").attr("src", $(this).attr("src"));        
         $("#collection-upper-thumbnail-link").attr("href", nodeLink);
-        if ($("#collection-thumbnail-upper-container").css("display") == "none"){
+        $("#collection-thumbnail-upper-container").css("display", "none");        
+        if ($("#collection-thumbnail-upper-container").css("display") === "none") {
           $("#collection-thumbnail-upper-container").css("display", "block");
-          $("#collection-upper-thumbnail").animate({
-            height: height
-          },{
+          $("#collection-thumbnail-upper-container").animate({
+            width: width
+          }, {
             duration: 300,
-            complete: function(){
+            queue: false,            
+            complete: function() {
+              $(this).css('height', '');
               $("#collection-thumbnail-metainfo").html(metainfo);
-              $("#collection-thumbnail-metainfo").width(1.5*initialWidth);
+              $("#collection-thumbnail-metainfo").width(1.5 * initialWidth);
             },
-            step: function(now, fx){
-              var left = initialLeft - initialWidth*0.5*(now/lowHeight - 1) - padding;
-              var right = left + now + 2*padding;
-              var containerWidth = $("#collection-standart-thumbnail-view-container").width();
-              if (right >= containerWidth){
-                left = containerWidth - now;
-              }
-              if (left < 0){
+            step: function(now, fx) {             
+              var left = initialLeft - (now - initialWidth) * 0.5;
+              var height = initialHeight*now/initialWidth;
+              $(this).height(height);            
+              if (left < 0) {
                 left = 0;
               }
-              $("#collection-thumbnail-upper-container").css("left", left);
-              $("#collection-thumbnail-upper-container").width((now/lowHeight)*initialWidth);
+              var right = left + now;
+              var containerWidth = $("#collection-standart-thumbnail-view-container").width();
+              if (right >= containerWidth) {
+                left = containerWidth - now;
+              }
+              $(this).css("left", left);             
             }
           });
-      }else{
-        $("#collection-upper-thumbnail").attr("src", $(this).attr("src"));
-      }
-
-      },function(){
-
-      });
-  }
-}
+        } else {
+          $("#collection-upper-thumbnail").attr("src", $(this).attr("src"));
+        }
+      }, function() {});
+    }
+  };
 })(jQuery);
 
