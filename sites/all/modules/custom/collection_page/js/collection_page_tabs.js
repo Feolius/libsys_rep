ca949700da8cfde89936319c3aa1490442d26505
Bazhen Rzheutskiy <bazhen.rzheutsky@gmail.com>
2013-03-29 15:52:08 +0700

    add the storing of last active tab

--- sites/all/modules/custom/collection_page/js/collection_page_tabs.js
+++ sites/all/modules/custom/collection_page/js/collection_page_tabs.js
@@ -1,33 +1,52 @@
 (function ($) {
     Drupal.behaviors.collection_page_tabs = {
-        attach:function () {
+        attach: function () {
             var standard_view_tab_button = $("#tab-button-standard-view"),
                 stantard_view_types = $("#library-standard-view-types"),
                 library_tabs = $("#collection_page_tabs");
 
-            library_tabs.tabs();
-            if (Drupal.settings.collection_page.default_tab != false) {
-                library_tabs.tabs('select', Drupal.settings.collection_page.default_tab);
-            }
-            if (Drupal.settings.collection_page.disabled_tab != false && Drupal.settings.collection_page.disabled_tab > 0) {
-                library_tabs.tabs({disabled:[Drupal.settings.collection_page.disabled_tab] });
-            }
-            standard_view_tab_button.append(stantard_view_types);
-            stantard_view_types.hide();
 
-            standard_view_tab_button.mouseover(function(){
+            if (Drupal.settings.collection_page.default_tab != false && $.cookie('collection_page_last_time_active_tab').empty()) {
+                library_tabs.tabs('select', Drupal.settings.collection_page.default_tab);
+            } else {
+                library_tabs.tabs('select', parseInt($.cookie('collection_page_last_time_active_tab')));
+            }
+
+            library_tabs.tabs({
+                select: function (event, ui) {
+                    $.cookie('collection_page_last_time_active_tab', ui.index);
+                }
+            });
+
+            if (Drupal.settings.collection_page.disabled_tab != false && Drupal.settings.collection_page.disabled_tab > 0) {
+                library_tabs.tabs({disabled: [Drupal.settings.collection_page.disabled_tab] });
+            }
+
+            standard_view_tab_button.mouseenter(function () {
+                var left = $(this).position().left;
+                var top = $(this).position().top;
+                var width = $(this).width();
+                var height = $(this).height()
+                stantard_view_types.css("width", width);
+                stantard_view_types.css("top", top);
+                stantard_view_types.css("left", left);
+                stantard_view_types.css("height", height);
                 stantard_view_types.show();
             });
-            standard_view_tab_button.mouseout(function(){
+
+            stantard_view_types.mouseleave(function () {
                 stantard_view_types.hide();
             });
 
-            $("#library-standard-view-default-type").click(function(){
+            $("#library-standard-view-default-type").click(function () {
+                stantard_view_types.hide();
                 window.location = $("#library-standard-view-default-type").attr('link');
             });
-            $("#library-standard-view-thumbnail-type").click(function(){
+            $("#library-standard-view-thumbnail-type").click(function () {
+                stantard_view_types.hide();
                 window.location = $("#library-standard-view-thumbnail-type").attr('link');
             });
+
 
         }
     }
--- sites/all/modules/custom/collection_page/theme/collection-page.tpl.php
+++ sites/all/modules/custom/collection_page/theme/collection-page.tpl.php
@@ -1,8 +1,9 @@
-<div id="collection-page-views">
 <div id="collection_page_tabs">
+
   <ul>
     <?php foreach ($content as $view): ?>
-      <li><a id=<?php print 'tab-button-' . $view['#id']; ?> href= <?php print '#tab-' . $view['#id']; ?>><?php print $view['#title'];?></a></li>
+      <li id=<?php print 'tab-button-' . $view['#id']; ?>><a
+          href= <?php print '#tab-' . $view['#id']; ?>><?php print $view['#title'];?></a></li>
     <?php endforeach; ?>
   </ul>
   <?php foreach ($content as $view): ?>
@@ -12,9 +13,16 @@
       <?php endforeach; ?>
     </div>
   <?php endforeach; ?>
-</div>
-<div id="library-standard-view-types">
-    <div id="library-standard-view-default-type" class="library-standard-view-type" link="<?php print $default_url?>" >Default</div>
-    <div id="library-standard-view-thumbnail-type" class="library-standard-view-type" link="<?php print $thumbnail_url?>">Thumbnail</div>
-</div>
+  <div id="library-standard-view-types">
+    <ul>
+      <li id="library-standard-view-button"
+          class="library-standard-view-type"><?php print $content['#standard']['#title'];?></li>
+      <li id="library-standard-view-default-type" class="library-standard-view-type" link="<?php print $default_url ?>">
+        Default
+      </li>
+      <li id="library-standard-view-thumbnail-type" class="library-standard-view-type"
+          link="<?php print $thumbnail_url ?>">Thumbnail
+      </li>
+    </ul>
+  </div>
 </div>
