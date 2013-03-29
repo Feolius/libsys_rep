48f987f6dc7b0325491f708db1f037b7433530c4
feolius <feolius@gmail.com>
2013-03-30 01:06:59 +0700

    Removed accordion from collection page. Added choosing only one bundle be default. Adapted search block for library page

--- sites/all/modules/custom/collection_page/theme/collection-search-results.tpl.php
+++ sites/all/modules/custom/collection_page/theme/collection-search-results.tpl.php
@@ -2,11 +2,9 @@
   <ol class="collection-search-results-">
     <?php print $search_results; ?>
   </ol>
-  <?php if ($more_link): ?>
-<?php print $more_link ?>
-<?php else: ?>
-<?php print $pager ?>
-  <?php endif; ?>
+  <div id="collection-pager-container">
+    <?php print $pager ?>
+  </div>
 <?php else : ?>
   <h2><?php print t('Your search yielded no results'); ?></h2>
   <?php print search_help('search#noresults', drupal_help_arg()); ?>
