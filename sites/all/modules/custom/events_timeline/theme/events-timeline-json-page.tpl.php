e56a324054cecec7590a03e240e9f7254d8b7e06
Bazhen Rzheutskiy <bazhen.rzheutsky@gmail.com>
2013-04-04 13:31:46 +0700

    update the openlayers module (warning! now this module require proj4js module, which was downloaded too)

---
+++ sites/all/modules/custom/events_timeline/theme/events-timeline-json-page.tpl.php
@@ -0,0 +1,14 @@
+<html>
+    <body>
+        <div id="timeline-embed"></div>
+        <script type="text/javascript">
+            var timeline_config = {
+                width: '100%',
+                height: '100%',
+                source: '<?php print $source ?>',
+                start_at_slide: <?php print $start_slide ?>
+            }
+        </script>
+        <script type="text/javascript" src="<?php print $timeline_lib_url ?>"></script>
+    </body>
+</html>
