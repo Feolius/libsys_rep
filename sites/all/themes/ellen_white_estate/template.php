053539a43ae4870bdd85180d13b5929e56bb20e8
Olga Drabashevskaya <drabashevskaya@gmail.com>
2013-04-11 17:41:28 +0700

    date field

--- sites/all/themes/ellen_white_estate/template.php
+++ sites/all/themes/ellen_white_estate/template.php
@@ -3,6 +3,7 @@
  * Override or insert variables into the node template.
  */
 function ellen_white_estate_preprocess_node(&$vars) {
+  kpr($vars);
   if ($vars["type"] == "files") {
     drupal_add_library('system', 'ui.tabs');
     drupal_add_js(drupal_get_path('theme', 'ellen_white_estate') . '/js/node_files_page_tabs.js');
