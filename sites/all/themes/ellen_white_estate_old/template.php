08143fab1db6cc4d902c426a71afaa03ebeaf2c6
Bazhen Rzheutskiy <bazhen.rzheutsky@gmail.com>
2013-04-17 15:45:36 +0700

    fix mistakes in old ellen_white_theme

--- sites/all/themes/ellen_white_estate_old/template.php
+++ sites/all/themes/ellen_white_estate_old/template.php
@@ -2,9 +2,9 @@
 /**
  * Override or insert variables into the node template.
  */
-function ellen_white_estate_preprocess_node(&$vars) {
+function ellen_white_estate_old_preprocess_node(&$vars) {
   if ($vars["type"] == "files") {
     drupal_add_library('system', 'ui.tabs');
-    drupal_add_js(drupal_get_path('theme', 'ellen_white_estate') . '/js/node_files_page_tabs.js');
+    drupal_add_js(drupal_get_path('theme', 'ellen_white_estate_old') . '/js/node_files_page_tabs.js');
   }
 }
