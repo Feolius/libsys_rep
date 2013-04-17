90b25ce93bacf658d49cfab3edc19d6769122931
Olga Drabashevskaya <drabashevskaya@gmail.com>
2013-04-17 12:33:02 +0700

    bug fix

--- sites/all/themes/ellen_white_estate/layouts/main/main.tpl.php
+++ sites/all/themes/ellen_white_estate/layouts/main/main.tpl.php
@@ -15,42 +15,70 @@
 <?php drupal_add_css(drupal_get_path('theme', 'corporateclean') . '/tweet_button.css')?>
   <div class="page">
     <div class="panel-panel panel-main">
-      <?php if (!empty($content['header_left']) || !empty($content['header_right'])): ?>
-      <div id="header">
-        <div id="header-inside">
-          <?php if (!empty($content['header_left'])): ?>
-            <div id="header-inside-left">
-              <?php print $content['header_left']; ?>
-            </div>
-          <?php endif; ?>
-          <?php if (!empty($content['header_search_block'])): ?>
-            <div id="header-inside-right">
-              <?php print $content['header_search_block']; ?>
-            </div>
-          <?php endif; ?>
+
+      <?php if (!empty($content['header_left']) || !empty($content['header_search_block'])): ?>
+        <!-- Header. -->
+        <div id="header">
+          <div id="header-inside">
+            <?php if (!empty($content['header_left'])): ?>
+              <div id="header-inside-left">
+                <?php print $content['header_left']; ?>
+              </div>
+            <?php endif; ?>
+            <?php if (!empty($content['header_search_block'])): ?>
+              <div id="header-inside-right">
+                <?php print $content['header_search_block']; ?>
+              </div>
+            <?php endif; ?>
+          </div>
         </div>
-      </div>
+        <!-- EOF: #header -->
        <?php endif; ?>
+
+      <?php if (!empty($content['header_menu'])): ?>
+        <!-- Header menu. -->
+        <div id="header-menu">
+          <div id="header-menu-inside">
+            <?php print $content['header_menu']; ?>
+          </div>
+        </div>
+        <!-- EOF: #header-menu -->
+      <?php endif; ?>
 
       <?php if ($content['content']): ?>
         <!-- Content. -->
         <div id="content">
           <div id="content-inside" class="inside">
-            <?php print $content['content']; ?>
-          </div><!-- EOF: #content-inside -->
-          <!-- EOF: #content -->
+            <div id="main">
+              <?php print $content['content']; ?>
+            </div>
+            <!-- EOF: #content -->
+          </div>
         </div>
       <?php endif; ?>
 
-      <?php if ($content['right_sidebar']): ?>
-        <?php print $content['right_sidebar']; ?>
-      <?php endif; ?>
-      <?php if (!empty($content['footer'])): ?>
+      <?php if (!empty($content['footer'])
+        || !empty($content['footer_left'])
+        || !empty($content['footer_right'])): ?>
+        <!-- Footer. -->
         <div id="footer-bottom">
+        <?php if (!empty($content['footer'])): ?>
           <div id="footer-bottom-inside">
             <?php print $content['footer']; ?>
+            <?php if (!empty($content['footer_left'])): ?>
+              <div id="footer-bottom-left">
+                <?php print $content['footer_left']; ?>
+              </div>
+            <?php endif; ?>
+            <?php if (!empty($content['footer_right'])): ?>
+              <div id="footer-bottom-right">
+                <?php print $content['footer_right']; ?>
+              </div>
+            <?php endif; ?>
           </div>
+          <?php endif; ?>
         </div>
+        <!-- EOF: #footer-bottom -->
       <?php endif; ?>
     </div>
   </div>
