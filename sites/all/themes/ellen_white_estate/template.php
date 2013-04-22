6188143ccf3b3a9f2875c42ac81a90e7f7ed1bca
Olga Drabashevskaya <drabashevskaya@gmail.com>
2013-04-22 14:34:44 +0700

    ctools language

--- sites/all/themes/ellen_white_estate/template.php
+++ sites/all/themes/ellen_white_estate/template.php
@@ -8,3 +8,13 @@ function ellen_white_estate_preprocess_node(&$vars) {
     drupal_add_js(drupal_get_path('theme', 'ellen_white_estate') . '/js/node_files_page_tabs.js');
   }
 }
+
+/**
+ * Preprocesses variables for html.tpl.php.
+ */
+function ellen_white_estate_preprocess_html(&$variables) {
+  //Delete the class 'no-sidebars'.
+  if ($key = array_search('no-sidebars', $variables['classes_array'])) {
+    unset($variables['classes_array'][$key]);
+  }
+}
