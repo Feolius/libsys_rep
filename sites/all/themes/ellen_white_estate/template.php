43adbef1f478119b8fddcee760a6f69d9f16f5d7
Olga Drabashevskaya <drabashevskaya@gmail.com>
2013-04-30 18:12:18 +0700

    date field

--- sites/all/themes/ellen_white_estate/template.php
+++ sites/all/themes/ellen_white_estate/template.php
@@ -37,3 +37,98 @@ function ellen_white_estate_preprocess_html(&$variables) {
     unset($variables['classes_array'][$key]);
   }
 }
+
+/**
+ * Preprocesses variables for node--people-full.tpl.php.
+ */
+function ellen_white_estate_preprocess_node__people_full(&$vars) {
+  $node = $vars['node'];
+  $vars['full_name_people'] = '';
+  if (!empty($node->title)) {
+    $vars['full_name_people'] .= "{$node->title} ";
+  }
+  if (!empty($node->field_people_first_name)) {
+    $vars['full_name_people'] .= "{$node->field_people_first_name[LANGUAGE_NONE][0]['safe_value']} ";
+  }
+  if (!empty($node->field_people_middle_name)) {
+    $vars['full_name_people'] .= "{$node->field_people_middle_name[LANGUAGE_NONE][0]['safe_value']} ";
+  }
+  if (!empty($node->field_people_last_name)) {
+    $vars['full_name_people'] .= "{$node->field_people_last_name[LANGUAGE_NONE][0]['safe_value']} ";
+  }
+  if (!empty($node->field_people_maiden_name)) {
+    $vars['full_name_people'] .= "({$node->field_people_maiden_name[LANGUAGE_NONE][0]['safe_value']})";
+  }
+  if (!empty($node->field_people_degree)) {
+    $vars['full_name_people'] .= ", {$node->field_people_degree[LANGUAGE_NONE][0]['safe_value']} ";
+  }
+}
+
+/**
+ * Preprocesses variables for node--files-full.tpl.php.
+ */
+function ellen_white_estate_preprocess_node__files_full(&$vars) {
+  $node = $vars['node'];
+  $primary_media = $node->field_files_primary_media[LANGUAGE_NONE][0]['value'];
+  switch ($primary_media) {
+    case 'video':
+      $vars['output'] = _ellen_white_estate_preprocess_node__files_primary_video($vars);
+      break;
+
+    case 'image':
+      $vars['output'] = _ellen_white_estate_preprocess_node__files_primary_image($vars);;
+      break;
+  }
+}
+
+/**
+ * Preprocesses variables for files with primary media video.
+ */
+function _ellen_white_estate_preprocess_node__files_primary_video($vars) {
+  $output = array();
+  if ($vars['content']['field_files_youtube_media']) {
+    $output['video'] = $vars['content']['field_files_youtube_media'];
+  }
+  if ($vars['content']['field_files_description']) {
+    $output['description'] = $vars['content']['field_files_description'];
+  }
+  if ($vars['content']['field_video_date_taken']) {
+    $output['date_taken'] = $vars['content']['field_video_date_taken'];
+  }
+  if ($vars['content']['field_files_video_people']) {
+    $output['people'] = $vars['content']['field_files_video_people'];
+  }
+  if ($vars['content']['field_files_video_location']) {
+    $output['location'] = $vars['content']['field_files_video_location'];
+  }
+  if ($vars['content']['field_files_video_event']) {
+    $output['event'] = $vars['content']['field_files_video_event'];
+  }
+  return $output;
+}
+
+/**
+ * Preprocesses variables for files with primary media image.
+ */
+function _ellen_white_estate_preprocess_node__files_primary_image($vars) {
+  $output = array();
+  if ($vars['content']['field_files_image']) {
+    $output['image'] = $vars['content']['field_files_image'];
+  }
+  if ($vars['content']['field_files_description']) {
+    $output['description'] = $vars['content']['field_files_description'];
+  }
+  if ($vars['content']['field_files_image_people']) {
+    $output['people'] = $vars['content']['field_files_image_people'];
+  }
+  if ($vars['content']['field_files_image_location']) {
+    $output['location'] = $vars['content']['field_files_image_location'];
+  }
+  if ($vars['content']['field_files_image_event']) {
+    $output['event'] = $vars['content']['field_files_image_event'];
+  }
+  if ($vars['content']['field_image_date_taken']) {
+    $output['date_taken'] = $vars['content']['field_image_date_taken'];
+  }
+  return $output;
+}
