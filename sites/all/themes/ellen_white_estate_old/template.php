155207c3506a149e9b6667bdd43c553678a0e32d
feolius <feolius@gmail.com>
2013-05-08 01:47:49 +0700

    Added new jquery to old theme

--- sites/all/themes/ellen_white_estate_old/template.php
+++ sites/all/themes/ellen_white_estate_old/template.php
@@ -8,3 +8,15 @@ function ellen_white_estate_old_preprocess_node(&$vars) {
     drupal_add_js(drupal_get_path('theme', 'ellen_white_estate_old') . '/js/node_files_page_tabs.js');
   }
 }
+
+function ellen_white_estate_old_js_alter(&$javascript) {
+
+  $jquery_path = drupal_get_path('theme','ellen_white_estate_old') . '/js/jquery.min.js';
+  $javascript[$jquery_path] = $javascript['misc/jquery.js'];
+  $javascript[$jquery_path]['version'] = '1.9.1';
+  //$javascript[$jquery_path]['scope'] = 'header';
+  $javascript[$jquery_path]['data'] = $jquery_path;
+  //$javascript[$jquery_path]['weight'] = 19;
+  dpm($javascript);
+  unset($javascript['misc/jquery.js']);
+}
