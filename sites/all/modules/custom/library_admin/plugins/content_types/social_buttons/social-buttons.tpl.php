9e343356c436cdeac8f8eb33adc6220b14f0b741
Your Name <you@example.com>
2013-05-08 03:49:50 -0400

    template

---
+++ sites/all/modules/custom/library_admin/plugins/content_types/social_buttons/social-buttons.tpl.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * @file
+ * Library admin copyright block content type template.
+ */
+?>
+<!-- AddThis Button BEGIN -->
+  <div class="addthis_toolbox" style="">
+    <div class="social">
+      <a class="addthis_button_preferred_1 icon facebook"></a>
+      <a class="addthis_button_preferred_2 icon twitter"></a>
+      <a class="addthis_button_preferred_3 icon gplus"></a>
+      <a class="addthis_button_preferred_4 icon email"></a>
+      <a class="addthis_button_compact icon addthis"></a>
+     </div>
+  </div>
+  <script type="text/javascript">var addthis_config = {"data_track_addressbar":true};</script>
+  <script type="text/javascript" src="//s7.addthis.com/js/300/addthis_widget.js#pubid=ra-5189f38432a231ee"></script>
+<!-- AddThis Button END -->
