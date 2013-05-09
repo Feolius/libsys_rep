d21c70bdfef90e97f9412cd7235e08e8fd105cd9
Olga Drabashevskaya <drabashevskaya@gmail.com>
2013-05-09 16:46:42 +0700

    tabs

--- sites/all/themes/ellen_white_estate/js/library.js
+++ sites/all/themes/ellen_white_estate/js/library.js
@@ -5,27 +5,27 @@
  */
 (function ($, Drupal, window, document, undefined) {
 
-    // Function for download files.
-    function downloadFile(url) {
-      var link = document.createElement('a');
-      link.href = url;
+  // Function for download files.
+  function downloadFile(url) {
+    var link = document.createElement('a');
+    link.href = url;
 
-      if (link.download !== undefined) {
-        // Set HTML5 download attribute.
-        var fileName = url.substring(url.lastIndexOf('/') + 1, url.length);
-        link.download = fileName;
-      }
-
-      // Dispatching click event.
-      if (document.createEvent) {
-        var event = document.createEvent('MouseEvents');
-        event.initEvent('click', true, true);
-        link.dispatchEvent(event);
-        return true;
-      }
-      var query = '?download';
-      window.open(sUrl + query);
+    if (link.download !== undefined) {
+      // Set HTML5 download attribute.
+      var fileName = url.substring(url.lastIndexOf('/') + 1, url.length);
+      link.download = fileName;
     }
+
+    // Dispatching click event.
+    if (document.createEvent) {
+      var event = document.createEvent('MouseEvents');
+      event.initEvent('click', true, true);
+      link.dispatchEvent(event);
+      return true;
+    }
+    var query = '?download';
+    window.open(sUrl + query);
+  }
 
   Drupal.behaviors.libraryDownloadFile = {
     attach: function(context, settings) {
@@ -52,6 +52,32 @@
             ).focus();
             return false;
           })
+      })
+    }
+  }
+
+
+  Drupal.behaviors.libraryMoreInfoButton = {
+    attach: function(context, settings) {
+      $('a.information').once(function() {
+        $(this).click(function(event) {
+          event.preventDefault();
+          var info_block = $('div.information');
+          if (info_block.hasClass('hide-me')) {
+            info_block.removeClass('hide-me');
+          }
+          else {
+            info_block.addClass('hide-me');
+          }
+        })
+      })
+    }
+  }
+
+  Drupal.behaviors.libraryContentTabs = {
+    attach: function(context, settings) {
+      $('#file-tabs').once(function() {
+        $(this).tabs();
       })
     }
   }
