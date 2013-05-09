03f36abc541d2e5f2006da76d00a3c4fe60ff185
Olga Drabashevskaya <drabashevskaya@gmail.com>
2013-05-09 11:55:09 +0700

    features

---
+++ sites/all/themes/ellen_white_estate/js/library.js
@@ -0,0 +1,59 @@
+/**
+ *
+ * @file
+ * Library JS.
+ */
+(function ($, Drupal, window, document, undefined) {
+
+    // Function for download files.
+    function downloadFile(url) {
+      var link = document.createElement('a');
+      link.href = url;
+
+      if (link.download !== undefined) {
+        // Set HTML5 download attribute.
+        var fileName = url.substring(url.lastIndexOf('/') + 1, url.length);
+        link.download = fileName;
+      }
+
+      // Dispatching click event.
+      if (document.createEvent) {
+        var event = document.createEvent('MouseEvents');
+        event.initEvent('click', true, true);
+        link.dispatchEvent(event);
+        return true;
+      }
+      var query = '?download';
+      window.open(sUrl + query);
+    }
+
+  Drupal.behaviors.libraryDownloadFile = {
+    attach: function(context, settings) {
+      $('a.download').once(function() {
+        $(this).click(function(event) {
+          event.preventDefault();
+          var url = $('a.download').attr('href');
+          downloadFile(url);
+        })
+      })
+    }
+  }
+
+  Drupal.behaviors.libraryShareGooglePlusOneButton = {
+    attach: function(context, settings) {
+      $('.icon.gplus').once(function() {
+        $(this).click(function(event) {
+          var url = window.location.href;
+          event.preventDefault();
+            window.open(
+              'https://plus.google.com/share?url=' + url,
+              'popupwindow',
+              'scrollbars=yes,width=800,height=400'
+            ).focus();
+            return false;
+          })
+      })
+    }
+  }
+
+})(jQuery, Drupal, this, this.document);
