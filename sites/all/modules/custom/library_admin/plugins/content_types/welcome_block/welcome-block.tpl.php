6f1728fcd8fe08b6b347db7426d9f4164ee065f6
Olga Drabashevskaya <drabashevskaya@gmail.com>
2013-05-24 16:42:56 +0700

    Home page

---
+++ sites/all/modules/custom/library_admin/plugins/content_types/welcome_block/welcome-block.tpl.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * @file
+ * Library admin copyright block content type template.
+ */
+?>
+<?php if (!empty($copyright)): ?>
+  <div id="welcome-text">
+    <?php print $copyright; ?>
+  </div>
+<?php endif;?>
