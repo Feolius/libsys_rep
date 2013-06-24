4ac926ced0f3bb9c1284c38b5319ae52a4e10352
Olga Drabashevskaya <drabashevskaya@gmail.com>
2013-06-24 17:56:49 +0700

    Testing

--- sites/all/themes/ellen_white_estate/templates/node/node--files-full.tpl.php
+++ sites/all/themes/ellen_white_estate/templates/node/node--files-full.tpl.php
@@ -8,7 +8,6 @@
   <?php if (!empty($content['field_files_rating'])) : ?>
     <?php print drupal_render($content['field_files_rating']); ?>
   <?php endif; ?>
-  <div class="panel-separator"></div>
   <?php if (!empty($output)): ?>
     <?php foreach($output as $value): ?>
       <?php print drupal_render($value); ?>
