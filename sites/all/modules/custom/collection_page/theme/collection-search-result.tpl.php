334f9fc5398eb99bc638523d25d802868e8437c2
Your Name <you@example.com>
2013-07-25 07:02:06 -0400

    Some changes in css

--- sites/all/modules/custom/collection_page/theme/collection-search-result.tpl.php
+++ sites/all/modules/custom/collection_page/theme/collection-search-result.tpl.php
@@ -3,13 +3,14 @@
     <div class="collection-page-search-result-thumbnail-image">
       <?php print $image; ?>
     </div>
-  <?php endif; ?>
-  <h3 class="collection-page-search-result-title"<?php print $title_attributes; ?>>
-    <a href="<?php print $url; ?>"><?php print $title; ?></a>
-  </h3>
+  <?php endif; ?>  
   <?php if (!empty($snippet)): ?>
     <div class="collection-page-search-result-snippet"<?php print $content_attributes; ?>>
+      <h3 class="collection-page-search-result-title"<?php print $title_attributes; ?>>
+        <a href="<?php print $url; ?>"><?php print $title; ?></a>
+      </h3>
       <?php print $snippet; ?>
     </div>
   <?php endif; ?>
 </li>
+<li style="clear:both"></li>
