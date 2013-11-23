235f7155a8fb93fe5b0acdd3dd625100228e0960
feolius <feolius@gmail.com>
2013-11-23 22:20:54 +0700

    Get rid from useless things after flexpaper update

--- sites/all/modules/custom/book_field/theme/flexpaper-mode-switcher.tpl.php
+++
@@ -1,5 +0,0 @@
-<div class='flexpaper-switch-container'>
-  <span class='flexpaper-html5-switch-container'><a class='flexpaper-html5-switch flexpapaer-switch-link' href='<?php print $link_html5 ;?>'>HTML5</a></span>
-  <span class='flexpaper-flash-switch-container'><a class='flexpaper-flash-switch flexpapaer-switch-link' href='<?php print $link_flash ;?>'>FLASH</a></span>
-  <span class='flexpaper-html-switch-container'><a class='flexpaper-html-switch flexpapaer-switch-link' href='<?php print $link_html ;?>'>HTML</a></span>
-</div>
