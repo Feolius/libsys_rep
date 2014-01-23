14701df5b32a6a1a6f871c3954148f530583acc1
feolius <feolius@gmail.com>
2014-01-23 19:31:11 +0700

    Fixes in search. Now we don't have default content type in filters

--- sites/all/modules/custom/collection_page/theme/collection-page.tpl.php
+++ sites/all/modules/custom/collection_page/theme/collection-page.tpl.php
@@ -11,7 +11,11 @@
     <div id=<?php print 'tab-' . $view['#id']; ?>>
       <?php if (isset($view['#content'])): ?>
         <?php foreach ($view['#content'] as $view_content): ?>
-          <h3><?php print $bundle ?></h3>
+          <?php if ($bundle): ?>
+            <h3><?php print $bundle; ?></h3>
+          <?php else: ?>
+            <h3><?php print t("Library"); ?></h3>
+          <?php endif; ?>
           <?php print $view_content; ?>
         <?php endforeach; ?>
       <?php endif;?>
