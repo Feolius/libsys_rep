69b620fb97e7ca033f403f6b2e3e533e06bf1281
Dmitry <dmitry.larionov@qliff.com>
2015-05-28 15:18:04 +0600

    superfish, menu_import added

---
+++ sites/all/modules/custom/home_page/facebook_pull-feed.tpl.php
@@ -0,0 +1,33 @@
+<div>
+  <a href="https://www.facebook.com/ellenwhiteestate"><img title="Find us on Facebook" alt="Find us on Facebook" src="/sites/ellenwhite.org/files/egw_custom_images/facebook.png" style="height:41px; width:162px" />
+  </a>
+</div>
+<div class="clearfix"></div>
+<ul class="facebook-feed">
+<?php foreach ($items as $item): ?>
+  <li class="item">
+  <span class="facebook-feed-picture"><img alt="<?php echo $item->from->name; ?>" src="//graph.facebook.com/<?php echo $item->from->id; ?>/picture" /></span>
+  <span class="facebook-feed-from"><a href="//facebook.com/profile.php?id=<?php echo $item->from->id; ?>"><?php echo $item->from->name; ?></a></span>
+  <?php if (isset($item->story)): ?>
+      <span class="facebook-feed-story"><?php echo str_replace($item->from->name, '', $item->story); ?></span>
+    <?php endif; ?>
+    <span class="facebook-feed-message">
+      <?php if (isset($item->message)) echo $item->message; ?>
+      <?php if ($item->type === 'link'): ?>
+        <?php if (isset($item->description)) echo $item->description; ?>
+        <?php if (isset($item->name)) echo l($item->name, $item->link); ?>
+      <?php endif; ?>
+      <?php if (in_array($item->type, array('photo', 'video'))): ?>
+        <?php echo l('<img src="' . $item->picture . '" />', $item->link, array('html' => true, 'attributes' => array('target' => '_blank'))); ?>
+      <?php endif; ?>
+      <?php if ($item->type === 'question'): ?>
+        <?php echo $item->question; ?>
+        <?php foreach ($item->options as $option):?>
+          <span><?php echo $option; ?></span>
+        <?php endforeach; ?>
+      <?php endif; ?>
+    </span>
+    <span class="facebook-feed-time"><?php echo t('!time ago.', array('!time' => format_interval(time() - strtotime($item->created_time)))); ?></span>
+  </li>
+<?php endforeach; ?>
+</ul>
---
+++ sites/all/modules/custom/home_page/home_page_subscribe_block.tpl.php
@@ -0,0 +1,23 @@
+<table width="100%">
+  <tbody>
+  <tr>
+    <td>
+      <div>
+        <h3>My Life Today
+          A Daily Devotional
+          by Ellen G. White </h3>
+      </div>
+      <div></div>
+      <div></div>
+    </td>
+    <td>
+      <div>
+<h3> Thought for the Day
+  by Ellen G. White </h3>
+      </div>
+      <div></div>
+      <div></div>
+    </td>
+  </tr>
+  </tbody>
+</table>
